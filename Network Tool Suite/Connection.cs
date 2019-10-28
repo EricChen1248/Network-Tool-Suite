e51a9c0716e69cf1aff20d91647796f8231e5abf
Eric Chen <ericchen1248@gmail.com>
2019-10-28 10:13:13 +0800

    Working Base

---
+++ Network Tool Suite/Connection.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Network_Tool_Suite
+{
+    public class Connection
+    {
+        public int Port { get; private set; }
+        public IPAddress IP { get; private set; }
+
+        private const int PacketSize = 8096;
+        private TcpClient client;
+
+        private TcpListener server;
+
+        public Connection()
+        {
+            Port = 45645;
+            Console.ReadLine();
+        }
+
+        public void CreateServerClient()
+        {
+            server = new TcpListener(IPAddress.Any, Port);
+            server.Start();
+        }
+
+        public void ConnectToServer(string ipString)
+        {
+            IP = IPAddress.Parse(ipString);
+        }
+
+        public void SendStream(byte[] byteArray)
+        {
+            client = server.AcceptTcpClient();
+            var clientStream = client.GetStream();
+            var comp = Compress(byteArray);
+            clientStream.Write(comp, 0, comp.Length);
+            clientStream.Close();
+        }
+
+        public Image ReceiveStream()
+        {
+            client = new TcpClient(IP.ToString(), Port);
+
+            MemoryStream output = new MemoryStream();
+            var clientStream = client.GetStream();
+            using (DeflateStream dstream = new DeflateStream(clientStream, CompressionMode.Decompress))
+            {
+                dstream.CopyTo(output);
+            }
+            return Image.FromStream(output);
+        }
+
+        public static byte[] Compress(byte[] data)
+        {
+            MemoryStream output = new MemoryStream();
+            using (DeflateStream dstream = new DeflateStream(output, CompressionLevel.Optimal))
+            {
+                dstream.Write(data, 0, data.Length);
+            }
+            return output.ToArray();
+        }
+    }
+}
---
+++ Network Tool Suite/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace Network_Tool_Suite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly int ScreenLeft = (int) SystemParameters.VirtualScreenLeft;
+        private static readonly int ScreenTop = (int) SystemParameters.VirtualScreenTop;
+        private static readonly int ScreenWidth = (int) SystemParameters.VirtualScreenWidth / 4;
+        private static readonly int ScreenHeight = (int) SystemParameters.VirtualScreenHeight / 2;
+
+        private static Bitmap _bmp = new Bitmap(ScreenWidth, ScreenHeight);
+        private static readonly Graphics _g = Graphics.FromImage(_bmp);
+        public static MemoryStream Memory = new MemoryStream();
+        public DispatcherTimer Timer = new DispatcherTimer();
+
+        public static Connection Connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Connection = new Connection();
+        }
+
+        private void Start_Server(object sender, RoutedEventArgs e)
+        {
+            Connection.CreateServerClient();
+            Timer.Tick += SendScreen;
+            Timer.Interval = TimeSpan.FromSeconds(0.03);
+            Timer.Start();
+        }
+
+        private void Start_Client(object sender, RoutedEventArgs e)
+        {
+            Connection.ConnectToServer(ipTextBox.Text);
+            Timer.Tick += ShowScreen;
+            Timer.Interval = TimeSpan.FromSeconds(0.03);
+            Timer.Start();
+        }
+
+        private void ShowScreen(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            var oldBmp = new Bitmap(_bmp);
+            _bmp = (Bitmap) Connection.ReceiveStream();
+
+            OverlayBitmap(oldBmp, _bmp);
+
+            Memory.Position = 0;
+            _bmp.Save(Memory, ImageFormat.Png);
+            Memory.Position = 0;
+
+            var bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+            bitmapImage.StreamSource = Memory;
+            bitmapImage.EndInit();
+
+            ImageViewer.Source = bitmapImage;
+            Timer.Start();
+        }
+
+        private void SendScreen(object sender, EventArgs e)
+        {
+            Timer.Stop();
+
+            var oldBmp = (Bitmap) _bmp.Clone();
+            _g.CopyFromScreen(ScreenLeft, ScreenTop, 0, 0, _bmp.Size);
+            GetDifferenceImage(_bmp, oldBmp);
+            oldBmp.Save(Memory, ImageFormat.Bmp);
+            Connection.SendStream(Memory.ToArray());
+            oldBmp.Dispose();
+
+            Timer.Start();
+        }
+
+        public static unsafe void GetDifferenceImage(Bitmap image1, Bitmap image2)
+        {
+            Rectangle bounds = new Rectangle(0, 0, image1.Width, image1.Height);
+            var bmpDataA = image1.LockBits(bounds, ImageLockMode.ReadWrite, image1.PixelFormat);
+            var bmpDataB = image2.LockBits(bounds, ImageLockMode.ReadWrite, image2.PixelFormat);
+
+            int height = ScreenHeight;
+            int npixels = height * bmpDataA.Stride / 4;
+            int* pPixelsA = (int*)bmpDataA.Scan0.ToPointer();
+            int* pPixelsB = (int*)bmpDataB.Scan0.ToPointer();
+
+            for (int i = 0; i < npixels; ++i)
+            {
+                if (pPixelsA[i] == pPixelsB[i])
+                {
+                    pPixelsB[i] = Color.Transparent.ToArgb();
+                }
+                else
+                {
+                    pPixelsB[i] = pPixelsA[i];
+                }
+            }
+
+            image1.UnlockBits(bmpDataA);
+            image2.UnlockBits(bmpDataB);
+        }
+
+        public static unsafe void OverlayBitmap(Bitmap image1, Bitmap image2)
+        {
+            Rectangle bounds = new Rectangle(0, 0, image1.Width, image1.Height);
+            var bmpDataA = image1.LockBits(bounds, ImageLockMode.ReadWrite, image1.PixelFormat);
+            var bmpDataB = image2.LockBits(bounds, ImageLockMode.ReadWrite, image2.PixelFormat);
+
+            int height = ScreenHeight;
+            int npixels = height * bmpDataA.Stride / 4;
+            int* pPixelsA = (int*)bmpDataA.Scan0.ToPointer();
+            int* pPixelsB = (int*)bmpDataB.Scan0.ToPointer();
+
+            for (int i = 0; i < npixels; ++i)
+            {
+                if (pPixelsB[i] == Color.Transparent.ToArgb())
+                {
+                    pPixelsB[i] = pPixelsA[i];
+                }
+            }
+
+            image1.UnlockBits(bmpDataA);
+            image2.UnlockBits(bmpDataB);
+        }
+    }
+}
